0e751d69710cb7b7d31ddc991a4d873df9fe3c68
blackandblack <andrew.lpuig@gmail.com>
2017-08-10 17:13:01 -0400

    Init.

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+const gulp = require('gulp');
+const gulpAmpValidator = require('gulp-amphtml-validator');
+
+const paths = {
+  src: '*.html'
+};
+
+gulp.task('amphtml:validate', () => {
+  return gulp.src(paths.src)
+    .pipe(gulpAmpValidator.validate())
+    .pipe(gulpAmpValidator.format())
+    .pipe(gulpAmpValidator.failAfterError());
+});
+
+gulp.task('default', ['amphtml:validate'], function () {
+});
